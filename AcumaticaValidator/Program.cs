99d950b9ac171c9c61e07358419a292d253b3f8f
AAntoque <anjoantoque@gmail.com>
2024-02-27 06:18:17 +0800

    add ignore file & color color sa text

--- AcumaticaValidator/Program.cs
+++ AcumaticaValidator/Program.cs
@@ -19,6 +19,7 @@ namespace AcumaticaValidator
 		public const string XMLFILENAME = "project.xml";
 		public const string BINFOLDERNAME = "Bin";
 		public const string TEMPFOLDERNAME = "Temp";
+		public const string FIELDSIGNOREFILENAME = "fields.ignore";
 
 		public static string EXTRACTEDZIPPATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 		static void Main()
@@ -37,7 +38,6 @@ namespace AcumaticaValidator
 
 			Console.WriteLine("Enter Package Path: ");
 			zipPath = Console.ReadLine();
-
 			bool validZip = ValidZipPath(zipPath);
 
 			//get the bin path from the supplied zip path
@@ -82,7 +82,7 @@ namespace AcumaticaValidator
 
 				if (!string.IsNullOrEmpty(outputPath))
 				{
-					ValidateProjectXML(outputPath);
+					ValidateProjectXML(zipPath, outputPath);
 				}
 			}
 			else
@@ -94,7 +94,147 @@ namespace AcumaticaValidator
 		}
 
 		#region Method
+		protected static void ValidateProjectXML(string zipPath, string outputZipPath)
+		{
+			try
+			{
+				XmlDocument doc = new XmlDocument();
+				doc.Load(Path.Combine(outputZipPath, XMLFILENAME));
 
+				List<XmlNode> graphs = new List<XmlNode>();
+				List<XmlNode> tables = new List<XmlNode>();
+
+				foreach (XmlNode node in doc?.DocumentElement?.ChildNodes)
+				{
+					switch (node.Name)
+					{
+						case "Graph":
+							graphs.Add(node);
+							break;
+						case "Table":
+							tables.Add(node);
+							break;
+						default:
+							break;
+					}
+				}
+
+				string regExPattern = @"usr\w+";
+				List<string> usrFields = new List<string>();
+
+				//Get all usr fields from all classes in the package
+				foreach (XmlNode graph in graphs)
+				{
+					string innerText = graph.InnerText;
+
+					Regex regex = new Regex(regExPattern);
+
+					var result = regex.Matches(innerText);
+
+					foreach (Match res in result)
+					{
+						string s = res?.Value;
+						if (!string.IsNullOrEmpty(s) && !usrFields.Contains(s))
+						{
+							usrFields.Add(s);
+						}
+					}
+				}
+
+				usrFields.AddRange(GetUsrFieldsFromDLL(outputZipPath));
+
+				List<string> colNames = new List<string>();
+
+				//Get usr fields in tables
+				foreach (XmlNode table in tables)
+				{
+					foreach (XmlNode col in table.ChildNodes)
+					{
+						var colName = col.Attributes?.GetNamedItem("ColumnName")?.Value;
+
+						if (!string.IsNullOrEmpty(colName))
+						{
+							colNames.Add(colName);
+						}
+					}
+				}
+
+
+				List<string> ignoreFields = GetIgnoreFields(zipPath);
+
+				List<string> missingFields = new List<string>();
+
+				//Validate USR Fields
+				foreach (string usrField in usrFields)
+				{
+					if (!colNames.Where(t => t.ToLower() == usrField.ToLower()).Any())
+					{
+						//skip field if listed in ignore fields
+						if (ignoreFields.Where(t => t.ToLower() ==usrField.ToLower()).Any()) continue;
+
+						if (!missingFields.Where(t => t.ToLower() == usrField.ToLower()).Any())
+						{
+							missingFields.Add(usrField);
+						}
+					}
+				}
+
+				if (missingFields.Any())
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("\nWarning: Possible missing field:\n" + string.Join("\n", missingFields));
+					Console.ResetColor();
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine("Everythin looks good. Goodluck sa pag publish. HAHAHAHA");
+					Console.ResetColor();
+				}
+
+				DeleteDirectory(outputZipPath);
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				ResetApp();
+			}
+
+		}
+
+		protected static List<string> GetUsrFieldsFromDLL(string outputZipPath)
+		{
+			List<string> fields = new List<string>();
+
+			//get assemblies in directory.
+			string folder = CopyFilesToTempFolder(Path.Combine(outputZipPath, BINFOLDERNAME));
+			var files = Directory.GetFiles(folder, "*.dll").Where(t => !Path.GetFileName(t).StartsWith("PX")).ToList();
+			//load each assembly.
+			foreach (string file in files)
+			{
+				var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
+
+				foreach (var type in assembly.GetTypes())
+				{
+					if (!type.IsClass) continue;
+					//Console.WriteLine(type?.BaseType?.Name);
+					if (!(type?.BaseType?.Name.StartsWith("PXCacheExtension")) ?? true) continue;
+
+					foreach (var prop in type.GetProperties())
+					{
+						if (fields.Contains(prop.Name) || !(prop.Name.ToLower().StartsWith("usr"))) continue;
+
+						fields.Add(prop.Name);
+					}
+				}
+			}
+
+			return fields;
+		}
+		#endregion
+
+		#region Helpers
 		protected static bool ValidPath(string path)
 		{
 			bool result = false;
@@ -189,161 +329,10 @@ namespace AcumaticaValidator
 			}
 		}
 
-		protected static void ValidateProjectXML(string outputZipPath)
-		{
-			try
-			{
-				XmlDocument doc = new XmlDocument();
-				doc.Load(Path.Combine(outputZipPath, XMLFILENAME));
-
-				List<XmlNode> graphs = new List<XmlNode>();
-				List<XmlNode> tables = new List<XmlNode>();
-
-				foreach (XmlNode node in doc?.DocumentElement?.ChildNodes)
-				{
-					switch (node.Name)
-					{
-						case "Graph":
-							graphs.Add(node);
-							break;
-						case "Table":
-							tables.Add(node);
-							break;
-						default:
-							break;
-					}
-				}
-
-				string regExPattern = @"usr\w+";
-				List<string> usrFields = new List<string>();
-
-				//Get all usr fields from all classes in the package
-				foreach (XmlNode graph in graphs)
-				{
-					string innerText = graph.InnerText;
-
-					Regex regex = new Regex(regExPattern);
-
-					var result = regex.Matches(innerText);
-
-					foreach (Match res in result)
-					{
-						string s = res?.Value;
-						if (!string.IsNullOrEmpty(s) && !usrFields.Contains(s))
-						{
-							usrFields.Add(s);
-						}
-					}
-				}
-
-				usrFields.AddRange(GetUsrFieldsFromDLL(outputZipPath));
-
-				List<string> colNames = new List<string>();
-
-				//Get usr fields in tables
-				foreach (XmlNode table in tables)
-				{
-					foreach (XmlNode col in table.ChildNodes)
-					{
-						var colName = col.Attributes?.GetNamedItem("ColumnName")?.Value;
-
-						if (!string.IsNullOrEmpty(colName))
-						{
-							colNames.Add(colName);
-						}
-					}
-				}
-
-				List<string> missingFields = new List<string>();
-
-				//Validate USR Fields
-				foreach (string usrField in usrFields)
-				{
-					if (!colNames.Where(t => t.ToLower() == usrField.ToLower()).Any())
-					{
-						if (!missingFields.Where(t => t.ToLower() == usrField.ToLower()).Any())
-						{
-							missingFields.Add(usrField);
-						}
-					}
-				}
-
-				if (missingFields.Any())
-				{
-					Console.WriteLine("\nWarning: Possible missing field:\n" + string.Join("\n", missingFields));
-				}
-				else
-				{
-					Console.WriteLine("Everythin looks good. Goodluck sa pag publish. HAHAHAHA");
-				}
-
-				DeleteDirectory(outputZipPath);
-
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				ResetApp();
-			}
-
-		}
-
-		protected static List<string> GetUsrFieldsFromDLL(string outputZipPath)
-		{
-			List<string> fields = new List<string>();
-
-			//get assemblies in directory.
-			string folder = CopyFilesToTempFolder(Path.Combine(outputZipPath, BINFOLDERNAME));
-			var files = Directory.GetFiles(folder, "*.dll").Where(t => !Path.GetFileName(t).StartsWith("PX")).ToList();
-			//load each assembly.
-			foreach (string file in files)
-			{
-				var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
-
-				foreach (var type in assembly.GetTypes())
-				{
-					if (!type.IsClass) continue;
-					//Console.WriteLine(type?.BaseType?.Name);
-					if (!(type?.BaseType?.Name.StartsWith("PXCacheExtension")) ?? true) continue;
-
-					foreach (var prop in type.GetProperties())
-					{
-						if (fields.Contains(prop.Name) || !(prop.Name.ToLower().StartsWith("usr"))) continue;
-
-						fields.Add(prop.Name);
-					}
-				}
-			}
-
-			return fields;
-		}
-
-		protected static string CopyFilesToTempFolder(string sourcePath)
-		{
-			string outputPath = Path.Combine(EXTRACTEDZIPPATH, OUTPUTFOLDERNAME, TEMPFOLDERNAME, Guid.NewGuid().ToString());
-
-			try
-			{
-				if(!Directory.Exists(outputPath))
-				{
-					Directory.CreateDirectory(outputPath);
-				}
-
-				CopyFilesRecursively(sourcePath, outputPath);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				ResetApp();
-			}
-
-			return outputPath;
-		}
-
 		protected static void ResetApp()
 		{
 			Console.WriteLine("\n");
-			
+
 			Main(true);
 		}
 
@@ -383,6 +372,49 @@ namespace AcumaticaValidator
 			{
 				File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
 			}
+		}
+
+		protected static string CopyFilesToTempFolder(string sourcePath)
+		{
+			string outputPath = Path.Combine(EXTRACTEDZIPPATH, OUTPUTFOLDERNAME, TEMPFOLDERNAME, Guid.NewGuid().ToString());
+
+			try
+			{
+				if (!Directory.Exists(outputPath))
+				{
+					Directory.CreateDirectory(outputPath);
+				}
+
+				CopyFilesRecursively(sourcePath, outputPath);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				ResetApp();
+			}
+
+			return outputPath;
+		}
+
+		protected static List<string> GetIgnoreFields(string zipPath)
+		{
+			List<string> fields = new List<string>();
+
+			var dir = Directory.GetParent(zipPath);
+
+			if(dir != null)
+			{
+				var ignoreFields = File.ReadAllLines(Path.Combine(dir.FullName, FIELDSIGNOREFILENAME));
+
+				foreach (var field in ignoreFields)
+				{
+					if (fields.Contains(field)) continue;
+
+					fields.Add(field.ToLower().Trim());
+				}
+			}	
+
+			return fields;
 		}
 		#endregion
 
